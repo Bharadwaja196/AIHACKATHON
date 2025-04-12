ef25e0be0ab9e3746467e20ebc26885d9573f8d2
Punith23luffy <punith.senji@gmail.com>
2025-04-12 11:53:29 +0530

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(BASE_DIR, ".."))
+
+DEFAULT_USER_ID = "user123"
+LOGGING_LEVEL = "DEBUG"
